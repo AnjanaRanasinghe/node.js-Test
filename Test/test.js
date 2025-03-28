82786c88ff2f5e2ce943bdb2094656ef9cae1385
Anjana Ranasinghe <anjana.20231160@iit.ac.lk>
2025-03-28 21:58:16 +0530

    Read from file

--- Test/test.js
+++ Test/test.js
@@ -1,3 +1,6 @@
 const fs = require('node:fs');
 
 fs.writeFileSync("text.txt", "HEllow");
+fs.appendFileSync("text.txt", " World!");
+const rd = fs.readFileSync("text.txt");
+console.log(rd.toString());
